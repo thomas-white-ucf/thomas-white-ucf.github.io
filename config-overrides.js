120973729eaaa7e6c33490d946a5aaa60b870e80
Thomas White <thomas.white.ucf@gmail.com>
2022-04-12 09:55:08 -0400

    module added to package, dep has breaking change..

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,6 @@
 /* config-overrides.js */
 /* eslint-disable react-hooks/rules-of-hooks */
-const { useBabelRc, override } = require('customize-cra');
+// const { useBabelRc, override } = require('customize-cra');
+import { useBabelRc, override } from 'customize-cra'
 
 module.exports = override(useBabelRc());
