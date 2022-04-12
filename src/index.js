4180440ccbfe9947388ffb0a2a2b0a8d0f5817e9
Thomas White <thomas.white.ucf@gmail.com>
2022-04-12 09:41:26 -0400

    test push from heroku folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 //
-import theme from "./theme";
+import theme from "./theme.js";
 //
 import App from "./App";
 
