74a98193ba6adf96cc5d2571ec272c0fb5b39d65
thomas-white-ucf <thomas.white.ucf@gmail.com>
2022-06-15 16:42:17 -0400

    push this auto commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,9 @@ import HomeDetail from "../components/HomeDetail";
 const HomePage = () => {
   return (
     <Grid item xs={12}>
+      {/* <a href="https://visitcount.itsvg.in">
+        <img src="https://visitcount.itsvg.in/api?id=twhite386&label=Pups&color=6&icon=4&pretty=true" />
+      </a> */}
       <HomeDetail />
     </Grid>
   );
